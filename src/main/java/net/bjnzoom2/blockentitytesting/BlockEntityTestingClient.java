55aa2e772e8380be823ee1ecc50b1febd611c8cb
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-28 20:29:53 +0800

    Initial commit

---
+++ src/main/java/net/bjnzoom2/blockentitytesting/BlockEntityTestingClient.java
@@ -0,0 +1,10 @@
+package net.bjnzoom2.blockentitytesting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockEntityTestingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
