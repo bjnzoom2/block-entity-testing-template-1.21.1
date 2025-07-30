1260dcb5e365f7407c222c4f22bd35a862284e40
bjnzoom2 <bjnzoom2@gmail.com>
2025-07-30 17:14:20 +0800

    Commit

--- src/main/java/net/bjnzoom2/blockentitytesting/BlockEntityTestingClient.java
+++ src/main/java/net/bjnzoom2/blockentitytesting/BlockEntityTestingClient.java
@@ -1,10 +1,14 @@
 package net.bjnzoom2.blockentitytesting;
 
+import net.bjnzoom2.blockentitytesting.block.ModBlocks;
+import net.bjnzoom2.blockentitytesting.block.entity.ModBlockEntities;
+import net.bjnzoom2.blockentitytesting.block.entity.renderer.PedestalBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class BlockEntityTestingClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockEntityRendererFactories.register(ModBlockEntities.PEDESTAL_BE, PedestalBlockEntityRenderer::new);
     }
 }
