8bfc305586f3ec0061a86b98dda47a1623a9db2b
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-01 15:54:38 +0800

    Commit

--- src/main/java/net/bjnzoom2/blockentitytesting/screen/ModScreenHandlers.java
+++ src/main/java/net/bjnzoom2/blockentitytesting/screen/ModScreenHandlers.java
@@ -1,6 +1,7 @@
 package net.bjnzoom2.blockentitytesting.screen;
 
 import net.bjnzoom2.blockentitytesting.BlockEntityTesting;
+import net.bjnzoom2.blockentitytesting.screen.custom.GrowthChamberScreenHandler;
 import net.bjnzoom2.blockentitytesting.screen.custom.PedestalScreenHandler;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.minecraft.registry.Registries;
@@ -13,6 +14,10 @@ public class ModScreenHandlers {
     public static final ScreenHandlerType<PedestalScreenHandler> PEDESTAL_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(BlockEntityTesting.MOD_ID, "pedestal_screen_handler"),
                     new ExtendedScreenHandlerType<>(PedestalScreenHandler::new, BlockPos.PACKET_CODEC));
+
+    public static final ScreenHandlerType<GrowthChamberScreenHandler> GROWTH_CHAMBER_SCREEN_HANDLER =
+            Registry.register(Registries.SCREEN_HANDLER, Identifier.of(BlockEntityTesting.MOD_ID, "growth_chamber_screen_handler"),
+                    new ExtendedScreenHandlerType<>(GrowthChamberScreenHandler::new, BlockPos.PACKET_CODEC));
 
     public static void registerScreenHandlers() {
         BlockEntityTesting.LOGGER.info("Registering Screen Handlers for " + BlockEntityTesting.MOD_ID);
