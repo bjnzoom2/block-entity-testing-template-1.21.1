977d8a8511f24a4af752e3c1486a860919d1cb5e
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-01 21:25:00 +0800

    Commit

--- src/main/java/net/bjnzoom2/blockentitytesting/BlockEntityTestingDataGenerator.java
+++ src/main/java/net/bjnzoom2/blockentitytesting/BlockEntityTestingDataGenerator.java
@@ -1,6 +1,7 @@
 package net.bjnzoom2.blockentitytesting;
 
 import net.bjnzoom2.blockentitytesting.datagen.ModModelProvider;
+import net.bjnzoom2.blockentitytesting.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -10,5 +11,6 @@ public class BlockEntityTestingDataGenerator implements DataGeneratorEntrypoint 
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/net/bjnzoom2/blockentitytesting/datagen/ModRecipeProvider.java
@@ -0,0 +1,39 @@
+package net.bjnzoom2.blockentitytesting.datagen;
+
+import net.bjnzoom2.blockentitytesting.block.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter recipeExporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.PEDESTAL)
+                .pattern("SSS")
+                .pattern(" S ")
+                .pattern("III")
+                .input('S', Items.SMOOTH_STONE)
+                .input('I', Items.IRON_BLOCK)
+                .criterion(hasItem(Items.SMOOTH_STONE), conditionsFromItem(Items.SMOOTH_STONE))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.GROWTH_CHAMBER)
+                .pattern("III")
+                .pattern("IDI")
+                .pattern("III")
+                .input('D', Items.DIAMOND_BLOCK)
+                .input('I', Items.IRON_BLOCK)
+                .criterion(hasItem(Items.IRON_BLOCK), conditionsFromItem(Items.IRON_BLOCK))
+                .offerTo(recipeExporter);
+    }
+}
