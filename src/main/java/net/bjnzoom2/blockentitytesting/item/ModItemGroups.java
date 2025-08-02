f6cf013529fd9fd90eb0b1e9658d7a0ee2464fcd
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-02 21:14:34 +0800

    Commit

--- src/main/java/net/bjnzoom2/blockentitytesting/item/ModItemGroups.java
+++ src/main/java/net/bjnzoom2/blockentitytesting/item/ModItemGroups.java
@@ -3,8 +3,6 @@ package net.bjnzoom2.blockentitytesting.item;
 import net.bjnzoom2.blockentitytesting.BlockEntityTesting;
 import net.bjnzoom2.blockentitytesting.block.ModBlocks;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -21,6 +19,7 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModBlocks.PEDESTAL);
                         entries.add(ModBlocks.GROWTH_CHAMBER);
+                        entries.add(ModBlocks.BIG_CRAFTING_TABLE);
                     }).build());
 
     public static void registerItemGroups() {BlockEntityTesting.LOGGER.info("Registering Item Groups for " + BlockEntityTesting.MOD_ID);
