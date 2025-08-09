119d90e20618448ef8be355b3b90e0153e5c7957
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-09 21:06:28 +0800

    Commit

--- src/main/java/net/bjnzoom2/blockentitytesting/item/ModItemGroups.java
+++ src/main/java/net/bjnzoom2/blockentitytesting/item/ModItemGroups.java
@@ -19,6 +19,7 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModBlocks.PEDESTAL);
                         entries.add(ModBlocks.GROWTH_CHAMBER);
+                        entries.add(ModBlocks.COMBINER);
                     }).build());
 
     public static void registerItemGroups() {BlockEntityTesting.LOGGER.info("Registering Item Groups for " + BlockEntityTesting.MOD_ID);
