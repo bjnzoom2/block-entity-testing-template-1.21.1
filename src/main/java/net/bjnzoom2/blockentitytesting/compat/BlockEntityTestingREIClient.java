b3ffe95f1ea85112cdef8aa38e06a97b1f4b1d4c
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-10 21:04:25 +0800

    Commit

--- src/main/java/net/bjnzoom2/blockentitytesting/compat/BlockEntityTestingREIClient.java
+++ src/main/java/net/bjnzoom2/blockentitytesting/compat/BlockEntityTestingREIClient.java
@@ -7,22 +7,28 @@ import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.bjnzoom2.blockentitytesting.block.ModBlocks;
+import net.bjnzoom2.blockentitytesting.recipe.CombinerRecipe;
 import net.bjnzoom2.blockentitytesting.recipe.GrowthChamberRecipe;
 import net.bjnzoom2.blockentitytesting.recipe.ModRecipes;
+import net.bjnzoom2.blockentitytesting.screen.custom.CombinerScreen;
 import net.bjnzoom2.blockentitytesting.screen.custom.GrowthChamberScreen;
 
 public class BlockEntityTestingREIClient implements REIClientPlugin {
     @Override
     public void registerCategories(CategoryRegistry registry) {
         registry.add(new GrowthChamberCategory());
+        registry.add(new CombinerCategory());
 
         registry.addWorkstations(GrowthChamberCategory.GROWTH_CHAMBER, EntryStacks.of(ModBlocks.GROWTH_CHAMBER));
+        registry.addWorkstations(CombinerCategory.COMBINER, EntryStacks.of(ModBlocks.COMBINER));
     }
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
         registry.registerRecipeFiller(GrowthChamberRecipe.class, ModRecipes.GROWTH_CHAMBER_TYPE,
                 GrowthChamberDisplay::new);
+        registry.registerRecipeFiller(CombinerRecipe.class, ModRecipes.COMBINER_TYPE,
+                CombinerDisplay::new);
     }
 
     @Override
@@ -30,5 +36,8 @@ public class BlockEntityTestingREIClient implements REIClientPlugin {
         registry.registerClickArea(screen -> new Rectangle(((screen.width - 176) / 2) + 78,
                         ((screen.height - 166) / 2) + 30, 20, 25), GrowthChamberScreen.class,
                 GrowthChamberCategory.GROWTH_CHAMBER);
+        registry.registerClickArea(screen -> new Rectangle(((screen.width - 176) / 2) + 78,
+                        ((screen.height - 168) / 2) + 30, 20, 25), CombinerScreen.class,
+                CombinerCategory.COMBINER);
     }
 }
